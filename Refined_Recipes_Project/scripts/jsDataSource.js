e6b34f4696d38fd9f72af5935dd8288c30622e83
willCodeForBurritos <aaronschlosser@gmail.com>
2016-06-21 03:55:01 +0000

    Add files via upload
    Uploading project I have been working on, named Refined Recipes

---
+++ Refined_Recipes_Project/scripts/jsDataSource.js
@@ -0,0 +1,91 @@
+var recipes = [{
+    RecipeID : 1,
+    RecipeName : "Bisquick Substitute",
+    ContributorName: "allrecipes.com",
+	Ingredients: "1 cup all-purpose flour,<br>1 1/2 teaspoons baking powder,<br>1/4 teaspoon salt,<br>1 tablespoon butter",
+	Directions: "Sift flour into a bowl; add baking powder and salt and whisk until fully incorporated. Cut butter into flour mixture using a pastry cutter until evenly combined.<br><br>*Shortening or oil can be used in place of the butter, if desired. If using melted butter, be sure to evenly disperse the butter as much as possible.",
+	Vegan: false,
+	GlutenFree: false
+},
+{
+    RecipeID : 2,
+    RecipeName : "Vegan Butter Substitute",
+    ContributorName: "www.forkandbeans.com",
+	Ingredients: "1 1/2c. melted refined coconut oil,<br>1/2 c. nondairy milk,<br>1/4 c. canola, grapeseed, or light olive oil,<br>1/2 tsp. sea salt,<br>2 teaspoons liquid lecithin",
+	Directions: "Place all the ingredients in a blender and process at medium speed for about 1 minute<br>Pour into container of choice--something made of silicone is great, as it will pop out easily, but any storage container will do (line it with wax paper first for easy removal)<br>Set it in the refrigerator for a few hours until hard or in the freezer to expedite hardening.",
+	Vegan: true,
+	GlutenFree: false
+},
+{
+    RecipeID : 3,
+    RecipeName : "Gluten Free Flour",
+    ContributorName: "minimalistbaker.com",
+	Ingredients: "1 1/2 cups (240 g) brown rice flour<br>1/2 cup (96 g) potato starch<br>1/4 cup (40 g) white rice flour<br>1/4 cup (30 g) tapioca flour<br>optional: 1 tsp xanthan gum (not necessary)",
+	Directions: "Blend together and store in a secure container in a dry place (I prefer a large mason jar).<br>This can be tricky because gluten free flours react differently in pretty much every recipe. But in general, use in place of all purpose or whole wheat flour in a 1:1 ratio. For extra binding (since there is no gluten) you can add a pinch of xanthan gum depending on the recipe, but I don't find it necessary.<br>This blend has worked wonders in muffins, cakes, quick breads, gingerbread cookies, corn bread, waffles, crepes and many other recipes.<br>I also have a Gluten Free Whole Grain Pancake Mix that works beautifully with an egg. And a Gluten Free Pizza Crust that utilizes a slight variation of this blend.",
+	Vegan: false,
+	GlutenFree: true
+},
+{
+    RecipeID : 4,
+    RecipeName : "Black Bean Burger",
+    ContributorName: "thepioneerwoman.com",
+	Ingredients: "2 cans (14.5 Each) Seasoned Black Beans<br>1 cup Seasoned Breadcrumbs<br>1/4 cup Grated White Onion<br>1 whole Egg<br>1/2 teaspoon Chili Powder<br>Salt And Pepper<br>Hot Sauce (I Used Choloula)<br>8 slices Swiss Cheese<br>Olive Oil, For Frying<br>Butter, For Frying And Grilling<br>4 whole Kaiser Rolls Or Good Hamburger Buns<br>Mayonnaise<br>Lettuce Or Other Greens<br>Sliced Tomato",
+	Directions: "Drain, but do not rinse, the black beans. Place them in a bowl and use a fork to mash them. Keep mashing until they're mostly broken up, but still have some whole beans visible. Add the breadcrumbs, onion, egg, chili powder, salt, pepper, and hot sauce. Stir until everything is combined, then let the mixture sit for 5 minutes. <br><br>Heat a tablespoon or two of olive oil with an equal amount of butter in a skillet over medium-low heat. Form the bean mixture into patties slightly larger than the buns you're using (the patties will not shrink when they cook.) Place the patties in the skillet and cook them about 5 minutes on the first side. Flip them to the other side, place 2 slices of cheese onto each patty, and continue cooking them for another 5 minutes, or until the burgers are heated through. (Place a lid on the skillet to help the cheese melt if needed.)<br><br>Grill the buns on a griddle with a little butter until golden. Spread the buns with mayonnaise and hot sauce, then place the patties on the buns. Top with lettuce and tomato, then pop on the lids!",
+	Vegan: false,
+	GlutenFree: false
+},
+{
+    RecipeID : 5,
+    RecipeName : "Homemade Vanilla Icing",
+    ContributorName: "bettycrocker.com",
+	Ingredients: "3 cups powdered sugar<br>1/3 cup butter, softened<br>1 1/2 teaspoons vanilla<br>1 to 2 tablespoons milk",
+	Directions: "1. In medium bowl, mix powdered sugar and butter with spoon or electric mixer on low speed. Stir in vanilla and 1 tablespoon of the milk.<br>2. Gradually beat in just enough remaining milk to make frosting smooth and spreadable. If frosting is too thick, beat in more milk, a few drops at a time. If frosting becomes too thin, beat in a small amount of powdered sugar. Frosts 13x9-inch cake generously, or fills and frosts an 8- or 9-inch two-layer cake.",
+	Vegan: false,
+	GlutenFree: true
+},
+{
+    RecipeID : 6,
+    RecipeName : "Healthy Oreos",
+    ContributorName: "chocolatecoveredkatie.com",
+	Ingredients: "3/4 cup spelt flour (or all-purpose, or gluten-free all-purpose) (120g)<br>1/4 cup plus 2 tbsp dutch cocoa powder (30g) (regular cocoa is fine; they just won’t taste as authentic)<br>1/4 cup plus 2 tbsp xylitol or sugar (or coconut sugar or evaporated cane juice) (78g)<br>1/4 tsp salt<br>1/4 tsp baking soda<br>1 tsp pure vanilla extract<br>1/4 cup vegetable or coconut oil (44g)<br>3 tbsp milk of choice (45g)<br>2 tbsp pure maple syrup (or agave) (30g)<br>Icing:<br>1/2 cup powdered sugar or Sugar-Free Powdered Sugar (55g)<br>1/2 tsp pure vanilla extract<br>1/4 cup coconut butter OR non-hydrogenated shortening (such as Spectrum) or Earth Balance coconut spread (58g)",
+	Directions: "Combine first 5 ingredients, and stir very well. In a separate bowl, combine all liquid ingredients for the cookies. Mix wet into dry to form a dough, then refrigerate 30 minutes before baking. Preheat oven to 300 degrees F. Put dough in a plastic bag, and smush into one big ball. Remove from bag, roll into a thin dough, and cut flat circles using a circle cutter or a circle-shaped lid. Bake on a greased cookie tray for 10 minutes; they’ll still look a little underdone when they come out of the oven, but that’s ok. Important: allow to cool 10 minutes before removing from the tray.<br><br>To make the filling, mix all filling ingredients in a small food processor (or patiently, with a fork). (If using coconut butter, make sure it is melted.) Put about a scant teaspoon-full (3g) on half of the cookie discs, then top each with remaining cookie discs and fridge so filling firms up. Store in the fridge so the filling stays hard.",
+	Vegan: true,
+	GlutenFree: true
+},
+{
+    RecipeID : 7,
+    RecipeName : "Gluten Free Pancakes",
+    ContributorName: "food.com",
+	Ingredients: "3⁄4 cup gluten-free flour (I use Gluten-Free Flour Blend)<br>1⁄2 teaspoon xanthan gum or 1⁄2 teaspoon guar gum<br>1 1⁄2 teaspoons baking powder<br>1⁄2 teaspoon salt<br>1 teaspoon sugar<br>2⁄3 cup milk<br>2 eggs<br>2 tablespoons cooking oil",
+	Directions: "Combine all dry ingredients in a bowl.<br>In a separate bowl, whisk together milk, eggs, and cooking oil.<br>Add milk mixture to the dry ingredients, and whisk until well blended.<br>Let stand for about 10 minutes.<br>Preheat ungreased, non-stick frying pan to medium heat.<br>Pour batter by spoonfuls onto the hot pan<br>Pancakes are ready to be turned over when the edges are no longer glossy, and the bubbles that pop stay open. To check the second side for doneness, you just have to lift an edge of the pancake up with a spatula and take a peek.",
+	Vegan: false,
+	GlutenFree: true
+},
+{
+    RecipeID : 8,
+    RecipeName : "Healthy Pecan Pumpkin Donuts",
+    ContributorName: "pure2raw.com",
+	Ingredients: "2 tbsp brown rice flour<br>1/2 cup potato starch<br>1/2 cup sorghum flour<br>1 tsp pumpkin pie spice<br>2 1/2 tsp baking powder<br>1/2 cup coconut sugar (or regular sugar)<br>1/2 cup pumpkin puree<br>3/4 cup buttermilk<br>3 tbsp coconut oil in liquid form",
+	Directions: "Add in coconut sugar, pumpkin puree, coconut oil into a bowl. Stir then add in your dry ingredients. Add in buttermilk and stir till combined. Pipe or scoop batter into donut pan. Or can use muffin pan if you do not have donut pan.  Bake at 350 for 16-20 minutes.<br>Once donuts are done remove from pan and let cool on cooling rack as you get your toppings ready.",
+	Vegan: false,
+	GlutenFree: false
+},
+{
+    RecipeID : 9,
+    RecipeName : "Vegan Egg Substitute",
+    ContributorName: "tipnut.com",
+	Ingredients: "1 TBS flax seed (ground)<br>3 TBS water",
+	Directions: "Method #1: Simmer together on top of stove for about 5 minutes until the consistency reaches a thick, egg-white like consistency.<br><br>Method #2: Blend ingredients together in a blender or food processor until the mixture is thick and creamy. Refrigerate before using",
+	Vegan: true,
+	GlutenFree: true
+},
+{
+    RecipeID : 10,
+    RecipeName : "Gluten Free Pizza Dough",
+    ContributorName: "epicurious.com",
+	Ingredients: "3/4 cup tapioca flour<br>1/2 cup white rice flour*<br>1/3 cup chickpea flour<br>1/3 cup sorghum flour<br>1 teaspoon xanthum gum<br>1 teaspoon fine sea salt<br>1/2 cup whole milk<br>2 1/4 teaspoons active dry yeast, from 1 (1/4-ounce) package<br>2 teaspoons sugar<br>2 large egg whites, lightly beaten<br>3 tablespoons plus 1 teaspoon extra-virgin olive oil",
+	Directions: "In bowl of electric mixer, whisk together tapioca flour, white rice flour, chickpea flour, sorghum flour, xanthum gum, and salt.<br>In small saucepan over moderate heat, stir together milk and 1/4 cup water and heat until warm but not hot to the touch, about 1 minute (the mixture should register between 105°F and 115° F on candy thermometer). Stir in yeast and sugar. Add milk–yeast mixture, egg whites, and 2 tablespoons oil to dry ingredients and, using paddle attachment, beat at medium speed, scraping bowl occasionally, until dough is very smooth and very thick, about 5 minutes.<br>Remove racks from oven, set pizza stone or heavy upturned baking sheet on bottom of oven, and preheat to 400°F. (Preheat at least 45 minutes if using pizza stone or 20 minutes if using baking sheet.)<br>Have ready two 12-inch squares parchment paper. Scrape half of dough onto each square and form each half into a ball. Coat each ball with 2 teaspoons oil, then use oiled fingertips to pat and stretch each ball into 9-inch-diameter round, 1/4 inch thick, with a 1/2-inch-thick border. Loosely cover rounds with plastic wrap and let rise in warm draft-free place until each pizza is about 10 inches in diameter, about 20 minutes.<br>Using baking peel, transfer 1 crust with parchment to preheated pizza stone and bake until top is puffed and firm and underside is crisp, 5 to 10 minutes. Using baking peel and discarding parchment paper, transfer baked crust to rack to cool. Bake second crust in same manner. (Baked crusts can be made ahead and frozen, wrapped in plastic wrap, up to 1 month. Thaw in 350°F oven until hot, 4 to 5 minutes, before topping and broiling.)",
+	Vegan: false,
+	GlutenFree: true
+}
+ ];
